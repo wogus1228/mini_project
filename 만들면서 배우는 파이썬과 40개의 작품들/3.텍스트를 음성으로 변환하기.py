600397decdedb305c75a7fe33ca26bbbce9c257c
wogus1228 <dlwogus1228@naver.com>
2022-07-26 23:56:02 +0900

    Create 3.텍스트를 음성으로 변환하기.py

---
+++ 만들면서 배우는 파이썬과 40개의 작품들/3.텍스트를 음성으로 변환하기.py
@@ -0,0 +1,22 @@
+# pip install gtts 
+# gtts : 문자를 음성으로 변환해주는 라이브러리
+
+# pip install playsound
+# playsound : mp3 파일을 파이썬에서 재생하기 위한 라이브러리
+
+from gtts import gTTS
+from playsound import playsound
+import os
+
+# 현재 경로를 기본 경로로 설정
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+file_path = 'sample.txt'
+with open(file_path, 'rt', encoding='UTF8') as f:
+    read_file = f.read()
+
+tts = gTTS(text=read_file, lang='ko')
+
+tts.save("myText.mp3")
+
+playsound("myText.mp3")
